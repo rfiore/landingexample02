8089d572cdc8566ddcf47d936207187539ab96ec
Riccardo <me@riccardofiore.com>
2019-04-15 15:32:49 +0200

    add product filter

---
+++ app/src/components/Products.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import style from '../App.module.scss';
+
+class Products extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			products: [],
+			searchKeys: ['est', 'omins'],
+			inputStr: ''
+		};
+
+		this.updateKeys = this.updateKeys.bind(this);
+	}
+
+	updateKeys(e) {
+		let newKeys = e.target.value.split(' ');
+		this.setState({
+			searchKeys: newKeys,
+			inputStr: e.target.value
+		}, ()=>{
+			console.log(this.state.searchKeys);
+		})
+	}
+
+
+	componentDidMount(){
+		let self = this;
+
+		fetch('https://jsonplaceholder.typicode.com/photos?_limit=500')
+			.then(results => {
+				return results.json();
+			}).then(data => {
+				self.setState({
+					products: [...data]
+				})
+			})
+	}
+
+  render() {
+		let self = this;
+
+		let productList = this.state.products.filter((item)=>{
+			let title = item.title;
+			let conditions = self.state.searchKeys;
+
+			return conditions.some(key => title.includes(key));
+		}).map((item)=>{
+			return (
+				<div key={item.id} className={style["col-md-4"]}>
+					<a href={item.url} target="_blank" className={style.product}>
+						<img src={item.url} className={style["img-fluid"]} />
+						<h4>{item.title}</h4>
+					</a>
+				</div>
+			)
+		});
+
+    return (
+      <div>
+				<h1>Products ({productList.length})</h1>
+				<div className={style.container}>
+					<div className={style.row}>
+						<div className={style["col-md-12"]}>
+							<div className={style["form-group"]}>
+								<input 
+									value={this.state.inputStr} 
+									onChange={this.updateKeys}
+									className={style["form-control"]} 
+									placeholder="Testo da ricercare"
+								/>
+							</div>
+						</div>
+						{productList}
+					</div>
+				</div>
+				
+      </div>
+    );
+  }
+}
+
+export default Products;
